c21bd23aec7e2a47b293b7d5c4c40c2e423e0349
Amogh <amogh.saxena15@gmail.com>
2020-10-17 12:49:24 +0530

    gfhgf

---
+++ build/precache-manifest.0310debd67df6981d0e44453172083de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95e10ce878f9e3f8cf2926c73719d57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4db47be335895cc06a0",
+    "url": "/static/css/main.01102b2d.chunk.css"
+  },
+  {
+    "revision": "abbab41e1a028d529bde",
+    "url": "/static/js/2.db107ec0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.db107ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4db47be335895cc06a0",
+    "url": "/static/js/main.94c53c73.chunk.js"
+  },
+  {
+    "revision": "3ced1a9c815820079f98",
+    "url": "/static/js/runtime-main.0f200393.js"
+  },
+  {
+    "revision": "3af1ab2a1691d5d2633ef0d0e6a52c39",
+    "url": "/static/media/bg.3af1ab2a.jpg"
+  }
+]);
